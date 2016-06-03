5d987663be63a3b4ecc29216a54186c14d522de7
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-06-03 14:31:58 -0700

    Validations

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
 
 var app = express();
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 var dbUri = 'mongodb://localhost:27017/api';
-// var dbConnection = mongoose.createConnection(dbUri);
-mongoose.connect(dbUri)
+var dbConnection = mongoose.createConnection(dbUri);
 
 var postSchema = new mongoose.Schema({
-  title: String,
-  text: String
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    match: /^([\w ,.!?]{1,100})$/
+  },
+  text: {type: String, required: true, max: 2000},
+  viewCounter: Number,
+  published: Boolean,
+  createdAd: {
+    type: Date,
+    default: Date.now(),
+    required: true
+  },
+  updatedAd: {
+    type: Date,
+    default: Date.now(),
+    required: true
+  }
 })
-var Post = mongoose.model('Post', postSchema, 'posts');
+var Post = dbConnection.model('Post', postSchema, 'posts');
 
 app.get('/', (req, res) => {
   res.send('ok')
@@ -23,6 +46,16 @@ app.get('/posts', (req, res, next) => {
   .then(posts => res.json(posts))
   .catch(next)
 })
+
+app.post('/posts', (req, res, next) => {
+  var post = new Post(req.body)
+  post.validate()
+  .then(() => post.save())
+  .then((results) => res.send(results))
+  .catch(error => res.status(400).send(error))
+})
+
+app.use(errorHandler());
 
 var server = require('http').createServer(app);
 server.listen(3000, () => {
